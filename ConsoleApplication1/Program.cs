1c03effcc7914bd601482fd601fca64da30119f4
juanonsoftware <juanonsoftware@github.com>
2015-11-20 14:13:52 +0700

    FlagsEnum testing

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    [Flags]
+    enum Colors
+    {
+        None = 0,
+        Red = 0x1,
+        Blue = 0x2,
+        Green = 0x4,
+        Yellow = 0x8
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var colors1 = Colors.Blue | Colors.Green | Colors.Red;
+
+            // Remove a Color from the list
+            var colors2 = colors1 & ~Colors.Green;
+
+            // Unboxing
+            var color3 = (Colors)(int)colors1;
+        }
+    }
+}
